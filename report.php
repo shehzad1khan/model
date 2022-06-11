0a299497beb5314479a7381e098e26c67d5ec360
shehzad1khan <commonemail61@gmail.com>
2022-06-11 18:44:24 +0500

    nothing added

--- report.php
+++ report.php
@@ -191,11 +191,6 @@
     <script src="js/styleSwitcher.js"></script>
 
   <!-- ***** Online script links ***** -->
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script> 
-
-<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
